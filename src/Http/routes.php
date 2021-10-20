12ce21275ef56b99c9ec9f3ebeb25a445b9304bd
Nabcellent <nabcellent.dev@gmail.com>
2021-10-20 14:39:52 +0300

    Commented Incomplete/Invalid routes

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -6,10 +6,10 @@ use Nabcellent\Kyanda\Http\Controllers\Controller;
 Route::group(['prefix' => 'kyanda/billing/', 'namespace' => Controller::class], function() {
     Route::get('balance', [Controller::class, 'accountBalance']);
     Route::post('transaction-status', [Controller::class, 'transactionStatus']);
-    Route::any('mobile-payout/create', '$routes');
-    Route::any('bank-payout/create', '$routes');
-    Route::any('checkout/create', '$routes');
+//    Route::any('mobile-payout/create', '$routes');
+//    Route::any('bank-payout/create', '$routes');
+//    Route::any('checkout/create', '$routes');
     Route::post('airtime/create', [Controller::class, 'airtimePurchase']);
-    Route::any('bill/create', '$routes');
+//    Route::any('bill/create', '$routes');
     Route::post('callback-url/create', [Controller::class, 'registerCallbackURL']);
 });
